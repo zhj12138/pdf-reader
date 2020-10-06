be78feff3c5e18599319b0b36da0d8bdb08a44b6
zhj12138 <1550223143@qq.com>
2020-10-06 22:14:33 +0800

    修改readme,发布阉割版2.0程序

--- mdEditor.py
+++ mdEditor.py
@@ -1,23 +1,23 @@
 from PyQt5.QtGui import QFont
-from PyQt5.QtWebEngineWidgets import QWebEngineView
+# from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtWidgets import QSplitter, QPlainTextEdit
 
-from mdConvert import md2html
+# from mdConvert import md2html
 
 
 class MdEditor(QSplitter):
     def __init__(self):
         super(MdEditor, self).__init__()
         self.writePart = QPlainTextEdit()
-        self.showPart = QWebEngineView()
-        self.showPart.setZoomFactor(1.5)
+        # self.showPart = QWebEngineView()
+        # self.showPart.setZoomFactor(1.5)
         self.writePart.setFont(QFont("", 13))
-        self.showPart.setMinimumWidth(20)
-        self.writePart.textChanged.connect(self.updateShowPart)
+        # self.showPart.setMinimumWidth(20)
+        # self.writePart.textChanged.connect(self.updateShowPart)
         self.addWidget(self.writePart)
-        self.addWidget(self.showPart)
+        # self.addWidget(self.showPart)
 
-    def updateShowPart(self):
-        text = self.writePart.toPlainText()
-        html = md2html(text)
-        self.showPart.setHtml(html)
+    # def updateShowPart(self):
+    #     text = self.writePart.toPlainText()
+    #     html = md2html(text)
+    #     self.showPart.setHtml(html)
