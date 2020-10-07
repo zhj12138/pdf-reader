956da2cc0f0ef4232fe9fab7d8299b373becd4b6
zhj12138 <1550223143@qq.com>
2020-10-07 09:46:53 +0800

    修复markdown编辑器

--- mdConvert.py
+++ mdConvert.py
@@ -1,9 +1,16 @@
 import markdown
-from pymdownx import superfences
-
+from pymdownx import superfences, magiclink, caret, betterem, mark, tasklist, tilde, inlinehilite, critic, highlight,\
+    _bypassnorm
+import mdx_math
 
 extensions = [
-    'markdown.extensions.extra',
+    'markdown.extensions.abbr',
+    'markdown.extensions.attr_list',
+    'markdown.extensions.def_list',
+    'markdown.extensions.fenced_code',
+    'markdown.extensions.footnotes',
+    'markdown.extensions.md_in_html',
+    'markdown.extensions.tables',
     'markdown.extensions.codehilite',
     'markdown.extensions.legacy_em',
     'markdown.extensions.toc',
@@ -14,7 +21,6 @@ extensions = [
     'markdown.extensions.nl2br',
     'markdown.extensions.sane_lists',
     'markdown.extensions.smarty',
-    'tables',
     'mdx_math',  # KaTeX数学公式，$E=mc^2$和$$E=mc^2$$
     # 'markdown_checklist.extension',  # checklist，- [ ]和- [x]
     'pymdownx.magiclink',  # 自动转超链接，
@@ -22,9 +28,14 @@ extensions = [
     'pymdownx.superfences',  # 多种块功能允许嵌套，各种图表
     'pymdownx.betterem',  # 改善强调的处理(粗体和斜体)
     'pymdownx.mark',  # 亮色突出文本
-    'pymdownx.highlight',  # 高亮显示代码
+    # 'pymdownx.highlight',  # 高亮显示代码
     'pymdownx.tasklist',  # 任务列表
     'pymdownx.tilde',  # 删除线
+    # 'pymdownx.emoji',
+    'pymdownx.inlinehilite',
+    # 'pymdownx.arithmatex',
+    'pymdownx.critic',
+    # 'markdown_markup_emoji.markup_emoji',
 ]
 Mycss = '''
 pre { line-height: 125%; margin: 0; }
